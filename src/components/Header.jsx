300571798d7143ff5e0d5bdf480901cad990bb6f
BennyhinnTitus <bennyhinntitus@gmail.com>
2025-07-31 00:09:18 +0530

    Created header, status section and languages list for Assembly: Endgame

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1 className="game-name">Assembly: Endgame</h1>
+            <p className="theme">Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Status.jsx
@@ -0,0 +1,10 @@
+const Status = () => {
+    return (
+        <div className="Status">
+            <span className="result">You Won!</span>
+            <span className="follow-up">Well done! 🎉</span>
+        </div>
+    )
+}
+
+export default Status
